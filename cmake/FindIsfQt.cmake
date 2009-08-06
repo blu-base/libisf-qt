4a7ad5baf59e0e2edd7bbfcdecd55ba3a6293d2a
Valerio Pilo <valerio@kmess.org>
2009-08-06 21:15:31 +0000

    isf-qt: Fixed the path of an include file.

--- cmake/FindIsfQt.cmake
+++ cmake/FindIsfQt.cmake
@@ -2,11 +2,11 @@
 # Find the Ink Serialized Format library and includes
 # This module defines
 #  ISFQT_FOUND       - If false, do not try to use ISF-Qt
-#  ISFQT_INCLUDE_DIR - where to find isfdrawing.h and friends
+#  ISFQT_INCLUDE_DIR - where to find isfqtdrawing.h and friends
 #  ISFQT_LIBRARIES   - the libraries needed to use ISF-Qt
 #  ISFQT_LIBRARY     - Where to find the ISF-Qt library (generally not used)
 
-FIND_PATH( ISFQT_INCLUDE_DIR isfdrawing.h /usr/include/isf-qt /usr/local/include/isf-qt )
+FIND_PATH( ISFQT_INCLUDE_DIR isfqtdrawing.h /usr/include/isf-qt /usr/local/include/isf-qt )
 FIND_LIBRARY( ISFQT_LIBRARY NAMES isf-qt PATH /usr/lib /usr/local/lib )
 
 # handle the QUIETLY and REQUIRED arguments and set ISFQT_FOUND to TRUE if
