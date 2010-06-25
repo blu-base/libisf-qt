f6514086ef9390a77f7e69ff818e7d8fe74b29d4
Valerio Pilo <valerio@kmess.org>
2010-06-26 00:30:08 +0200

    Unified the header comments in each files. (Apparently KDevelop considers the only first comment in the file as a header)

--- src/isfqt.cpp
+++ src/isfqt.cpp
@@ -4,9 +4,7 @@
  *                                                                         *
  *   Copyright (C) 2009 by Adam Goossens                                   *
  *   adam@kmess.org                                                        *
- ***************************************************************************/
-
-/***************************************************************************
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
  *   published by the Free Software Foundation; either version 2.1 of the  *
